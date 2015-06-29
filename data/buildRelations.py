bc8475ada42bbec9507abc64942eb2daeb9d277d
pbraunstein <pbraunstein12@gmail.com>
2015-06-29 12:30:01 -0400

    buildRelations uses Regex to pullout the translation of each record. Next, have it pull out all derived terms

--- data/buildRelations.py
+++ data/buildRelations.py
@@ -2,16 +2,60 @@
 
 from sys import exit
 from bs4 import BeautifulSoup
+import re
 
 # CONSTANTS
-INPUT = "filteredPhilsWayDE.xmlkeep"
+INPUT = "filteredPhilsWayDE.xml"
 OUTPUT = "verben.csv"
+NA = "N/A"
+
+class xmlRecord:
+    def __init__(self, record):
+        self.name = unicode(record.title.contents[0])
+        self.trans = self.getEnTrans(record.text)
+        self.isRoot = None
+        self.isSep = None
+        self.prefix = None
+        self.root = None
+        self.children = []
+
+    def __str__(self):
+        return self.name + "\n" + self.trans
+
+    # Parses the text of the XML record to get the En translation
+    def getEnTrans(self, text):
+
+        # e.g. {{UE|en|know}} Trying to pull out know,
+        # UE represents uppercase umlauted U
+        # Only matches the first definition (if there are more than one)
+        regEx = re.compile(ur'\{\{\u00dc\|en\|(\w*)\}\}', re.UNICODE)
+
+        match = regEx.search(text)
+
+        if match:
+            if match.group(1).strip() == '':  # Sometimes records are empty
+                return NA
+            else:
+                return match.group(1)
+        else:
+            return NA
 
 def main():
     soup = BeautifulSoup(open(INPUT), "xml")
     allPages = soup.findAll('page')
 
-    print len(allPages)
+    for page in allPages:
+        if page.title.contents[0] == 'suchen':
+            print xmlRecord(page)
+            break
+
+
+
+
+
+
+
+
 
 
 
